baf48f7025a2ecd9921e6436d1c7ae015f86506e
Yuvasri3010 <yuvasri6674@gmail.com>
2023-11-01 22:25:13 +0530

    first commit

---
+++ obj.js
@@ -0,0 +1,19 @@
+const obj = new Object();
+var c = new Object("Hi Friends");
+function myobject()
+{};
+myobject.prototype.name="Helo";
+
+console.log(c[Object])
+
+
+const numberObj = new Number(1);
+
+console.log(typeof numberObj);
+
+obj.Tamil = 42;
+obj.English = 78;
+obj.Social = 98;
+
+
+console.log(obj)
